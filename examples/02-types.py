77a8b9b8effc0a592793fd012c108e23d54f18c6
romickgr <romickgr@mail.ru>
2020-04-09 09:11:09 +0700

    messa

--- examples/02-types.py
+++ examples/02-types.py
@@ -84,6 +84,6 @@ person_construct = dict(first_name='John', age=53)
 john_name_key = person['first_name']
 john_name_getter = person.get('first_name')  # вернет False, если такого ключа нет
 
-person.keys()  # вернет названия полей
-person.values()  # вернет значения полей
+print(person.keys())  # вернет названия полей
+print(person.values())  # вернет значения полей
 person.items()  # вернет пары ключ-значение
--- src/day_02/00_hello_function.py
+++ src/day_02/00_hello_function.py
@@ -1,6 +1,14 @@
+
 """
 Пример программы для работы с функциями
 
 Сделать
 - функцию hello, которая выводит текст приветствия клиенту
 """
+
+
+clients = ['John', 'David', 'Kate', 'Alex']
+
+for user in clients:
+    print(f"Hello, {user}")
+
